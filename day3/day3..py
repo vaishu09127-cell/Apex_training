5aac81077e5351550251b2e1dd25f03f77da91e7
vaishu09127-cell <vaishu09127@gmail.com>
2026-09-25 13:23:03 +0530

    day3

---
+++ day3/day3..py
@@ -0,0 +1,8 @@
+java = float(input("enter the value:"))
+python=float(input("enter the value:"))
+Mysql=float(input("enter the value:"))
+print("total:",java+python+Mysql)
+print("average:",java +python+Mysql/3)
+print("percentage:",java+python+Mysql/3*100)
+print("remaining marks:",300-java+python+Mysql)
+
